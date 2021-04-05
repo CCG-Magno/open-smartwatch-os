a2f9f63e758d2b3c153758a0034221c0c9986b24
CCG-Magno <ciencias.computacion.games@gmail.com>
2021-04-05 17:56:48 -0400

    Added docs to buttons.cpp

--- src/hal/buttons.cpp
+++ src/hal/buttons.cpp
@@ -4,7 +4,9 @@
 #include "osw_pins.h"
 
 // Graphics2D screenBuffer(DISP_W, DISP_H, DISP_CHUNK_H);
-
+/**
+ * Setsup the HAL Buttons with their asigned Pins and their modes.
+ * */
 void OswHal::setupButtons(void) {
   rtc_gpio_deinit(GPIO_NUM_0);
   rtc_gpio_deinit(GPIO_NUM_10);
@@ -14,6 +16,10 @@ void OswHal::setupButtons(void) {
   pinMode(BTN_3, INPUT);
 }
 
+/**
+ * Waits for @param pin to switch into the specified @param targetState.
+ * Returns a long representation of the total time elapsed since the first call.
+ * */
 long waitUntil(OswHal* hal, uint8_t pin, uint8_t targetState) {
   long now = millis();
   delay(20);  // minimal debounce
@@ -40,27 +46,44 @@ long waitUntil(OswHal* hal, uint8_t pin, uint8_t targetState) {
   return millis() - now;
 }
 
+/**
+ * Waits for Button Pins to change state after reaching the minimum @param BTN_CLICK_TIMEOUT
+ * */
 void OswHal::checkButtons(void) {
   _btn1Down = false;
   _btn2Down = false;
   _btn3Down = false;
 
+  // Waits for the time to hit BTN_CLICK_TIMEOUT and for the pin of BTN_1 to go HIGH
   if (millis() - _lastBtn1Down > BTN_CLICK_TIMEOUT && digitalRead(BTN_1) == LOW) {
     _btn1Down = waitUntil(this, BTN_1, HIGH);
     _lastBtn1Down = millis();
   }
 
+  // Waits for the time to hit BTN_CLICK_TIMEOUT and for the pin of BTN_2 to go LOW
   if (millis() - _lastBtn2Down > BTN_CLICK_TIMEOUT && digitalRead(BTN_2) == HIGH) {
     _btn2Down = waitUntil(this, BTN_2, LOW);
     _lastBtn2Down = millis();
   }
 
+  // Waits for the time to hit BTN_CLICK_TIMEOUT and for the pin of BTN_2 to go LOW
   if (millis() - _lastBtn3Down > BTN_CLICK_TIMEOUT && digitalRead(BTN_3) == HIGH) {
     _btn3Down = waitUntil(this, BTN_3, LOW);
     _lastBtn3Down = millis();
   }
 }
 
+/**
+ * Returns long @param _btn1Down
+ * */
 long OswHal::btn1Down(void) { return _btn1Down; }
+
+/**
+ * Returns long @param _btn2Down
+ * */
 long OswHal::btn2Down(void) { return _btn2Down; }
+
+/**
+ * Returns bool @param _btn3Down
+ * */
 long OswHal::btn3Down(void) { return _btn3Down; }
